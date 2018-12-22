bd4ae7fa8c17c914dc36f7c7697bb51698a7050b
Weehan <deantiu56@gmail.com>
2018-12-22 10:30:57 +0800

    Added routes and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+
+  namespace :my do
+      resource :account, only: [:show, :update, :delete]
+      resources :todos
+  end
+
+  namespace :admin do
+      resources :users
+  end
+
+  resource :session, only: [:new, :create, :destroy]
 end
