c107c9e18228605a921c9de405bc34222d062a3b
Weehan <deantiu56@gmail.com>
2018-12-22 11:00:07 +0800

    'new user' page created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(params.require(:user).permit(:name, :email, :password, :admin))
+        if @user.save
+            redirect_to my_account_path
+        else
+            render :new
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
     has_many :todos
+
+    #Name must be present
+    validates :name, presence: true
+
+    #Email must be present and be unique
+    validates :email, presence: true
+    validates :email, uniqueness: true
+
+    #Password must be present
+    validates :password, presence: true
+
+    #admin must be present
+    validates :admin, presence: true
 end
