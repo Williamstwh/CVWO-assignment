1a68c4cefd5d153a467b5feaa27baa52142589ca
Weehan <deantiu56@gmail.com>
2018-12-23 22:29:08 +0800

    Set up todo page for users, added basic CSS

--- app/controllers/my/todos_controller.rb
+++ app/controllers/my/todos_controller.rb
@@ -9,28 +9,41 @@ class My::TodosController < ApplicationController
     end
 
     def new
-
+        @todo = current_user.todos.new
     end
 
     def create
+        @todo = current_user.todos.new(todo_params)
+        if @todo.save
+            redirect_to my_todos_path
+        else
+            render new_my_todo_path
+        end
     end
 
     def edit
     end
 
     def update
+        if @todo.update(todo_params)
+            redirect_to my_todo_path(@todo)
+        else
+            render edit_my_todo_path(@todo)
+        end
     end
 
     def destroy
+        @todo.destroy
+        redirect_to my_todos_path
     end
 
     private
 
     def find_todo
-        @todo = Todo.find(params[:id])
+        @todo = current_user.todos.find(params[:id])
     end
 
     def todo_params
-        params.require('todo').permit(:title, :text, :datetime)
+        params.require('todo').permit(:title, :description, :due)
     end
 end
